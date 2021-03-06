7c39fcace60ea21187ecf5ead0c97c977853eb71
Soham-pathak <soham.pathak@mail.utoronto.ca>
2021-03-05 20:20:11 -0500

    Collection-list completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch,Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>
+      Hats Page
+    </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div>
-      <HomePage></HomePage>
-    </div>
+      <div>
+       <Switch>
+       <Route exact={true} path='/' component={HomePage}/>
+       <Route path='/shop' component={ShopPage}/>
+       </Switch>
+      </div>
   );
 }
 
